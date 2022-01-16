7f85088ee827256e143c2725d04ad90f46215416
snajp <snajp@MSI>
2022-01-16 16:56:38 +0200

    Add project files.

---
+++ MauiApp1.App/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace MauiApp1.App.ViewModels
+{
+    internal class MainPageViewModel : ContentPage, INotifyPropertyChanged
+    {
+        public MainPageViewModel()
+        {
+            AllNotes = new ObservableCollection<string>();
+        }
+
+        public ObservableCollection<string> AllNotes { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        string theNote;
+
+        public string TheNote
+        {
+            get => theNote;
+            set
+            {
+                theNote = value;
+                var args = new PropertyChangedEventArgs(nameof(TheNote));
+
+                PropertyChanged?.Invoke(this, args);
+            }
+        }
+
+        public Command SaveCommand => new Command(() =>
+        {
+            AllNotes.Add(TheNote);
+
+            TheNote = string.Empty;
+        });
+
+        public Command EraseCommand => new Command(() =>
+        {
+            TheNote = string.Empty;
+        });
+    }
+}
